c6700e8fd46370e242bccc82ac41fe7874b96396
kor3nt <klaudiusz.jedrzejczyk@gmail.com>
2022-01-06 22:14:39 +0100

    changes

---
+++ src/main/java/me/duoly/bingo/Main.java
@@ -0,0 +1,19 @@
+package me.duoly.bingo;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+
+}

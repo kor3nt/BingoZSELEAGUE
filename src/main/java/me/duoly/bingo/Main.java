5939f53ceb61729066f63268527a975581909201
kor3nt <klaudiusz.jedrzejczyk@gmail.com>
2022-01-07 23:04:23 +0100

    stworzenie party, dodawanie kapitanow do teamu, error z scoreboardem

--- src/main/java/me/duoly/bingo/Main.java
+++ src/main/java/me/duoly/bingo/Main.java
@@ -1,19 +1,41 @@
 package me.duoly.bingo;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class Main extends JavaPlugin implements Listener {
+    public static Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
+
+    public static Team scoreTeamA = board.registerNewTeam("teamA");
+    public static Team scoreTeamB = board.registerNewTeam("teamB");
+
+    //Druzyna 1
+    public static String CapitanA;
+    public static List<String> TeamA = new ArrayList<String>();
+
+    //Druzyna 2
+    public static String CapitanB;
+    public static List<String> TeamB = new ArrayList<String>();
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
+        Party party = new Party();
+        getCommand("druzyna").setExecutor(party);
+        getCommand("party").setExecutor(party);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
-
 
 }
---
+++ src/main/java/me/duoly/bingo/Party.java
@@ -0,0 +1,197 @@
+package me.duoly.bingo;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+
+public class Party implements CommandExecutor {
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args ){
+        //admin - ustawianie kapitanow i wyswietlanie ich
+        setScoreBoard((Player) sender);
+
+        if(command.getName().equalsIgnoreCase("druzyna")){
+            Player admin = (Player) sender;
+            if(args.length == 0){
+                admin.sendMessage(ChatColor.YELLOW + "/druzyna add <teamA/teamB> <nick>" + ChatColor.WHITE + " - dodaje gracza na kapitana");
+                admin.sendMessage(ChatColor.YELLOW + "/druzyna show" + ChatColor.WHITE + " - pokazuje kapitanów");
+                return true;
+            }
+
+            if(args[0].equalsIgnoreCase("add")){
+                if(args.length == 1){
+                    admin.sendMessage(ChatColor.RED + "Poprawne użycie: /druzyna add <teamA/teamB> <nick>");
+                    return true;
+                }
+
+                if(args[1].equalsIgnoreCase("teamA")){
+                    Player player = Bukkit.getPlayer(args[2]);
+                    if(player != null && player.isOnline()){
+                        Main.CapitanA = args[2];
+                        Main.TeamA.add(Main.CapitanA);
+                        Main.scoreTeamA.addEntry(Main.CapitanA);
+                        admin.sendMessage(ChatColor.GREEN + "Pomyślnie dodałes kapitana teamu A");
+                        return true;
+                    }
+
+                    admin.sendMessage(ChatColor.RED + "Poprawne użycie: /druzyna add <teamA/teamB> <nick>");
+                    return true;
+                }
+                else if(args[1].equalsIgnoreCase("teamB")){
+                    Player player = Bukkit.getPlayer(args[2]);
+                    if(player != null && player.isOnline()){
+                        Main.CapitanB = args[2];
+                        Main.TeamB.add(Main.CapitanB);
+                        Main.scoreTeamB.addEntry(Main.CapitanB);
+                        admin.sendMessage(ChatColor.GREEN + "Pomyślnie dodałes kapitana teamu B");
+                        return true;
+                    }
+
+                    admin.sendMessage(ChatColor.RED + "Poprawne użycie: /druzyna add <teamA/teamB> <nick>");
+                    return true;
+                }
+            }
+            else if(args[0].equalsIgnoreCase("show")){
+                admin.sendMessage(ChatColor.AQUA + "TEAM A: " + ChatColor.WHITE + Main.CapitanA);
+                admin.sendMessage(ChatColor.AQUA + "TEAM B: " + ChatColor.WHITE + Main.CapitanB);
+                return true;
+            }
+            else{
+                admin.sendMessage(ChatColor.YELLOW + "/druzyna add <teamA/teamB> <nick>" + ChatColor.WHITE + " - dodaje gracza na kapitana");
+                admin.sendMessage(ChatColor.YELLOW + "/druzyna show" + ChatColor.WHITE + " - pokazuje kapitanów");
+                return true;
+            }
+        }
+
+        //user - komendy party
+        else if(command.getName().equalsIgnoreCase("party")){
+           Player player = (Player) sender;
+            if(args.length == 0){
+                player.sendMessage(ChatColor.BLUE + "Party:");
+                player.sendMessage(ChatColor.BLUE + "/party zapros <nick>");
+                player.sendMessage(ChatColor.BLUE + "/party usun <nick>");
+                player.sendMessage(ChatColor.BLUE + "/party list");
+                return true;
+            }
+
+            if(args[0].equalsIgnoreCase("zapros")){
+                if(args.length == 1){
+                    player.sendMessage(ChatColor.RED + "Poprawne użycie: /party zapros <nick>");
+                    return true;
+                }
+
+                if(player.getName().equalsIgnoreCase(Main.CapitanA)){
+                    if(Main.TeamA.contains(args[1])){
+                        player.sendMessage(ChatColor.RED + "Gracz już jest w party!");
+                        return true;
+                    }
+                    Player invitedPlayer = Bukkit.getPlayer(args[1]);
+                    if(invitedPlayer != null && invitedPlayer.isOnline()){
+                        Main.TeamA.add(invitedPlayer.getName());
+                        Main.scoreTeamA.addEntry(args[1]);
+                        player.sendMessage(ChatColor.GREEN + "Dodałeś gracza " + invitedPlayer.getName() + "do party");
+                        return true;
+                    }
+                    player.sendMessage(ChatColor.RED + "Takiego gracza nie ma!");
+                    return true;
+                }
+                else if(player.getName().equalsIgnoreCase(Main.CapitanB)){
+                    if(Main.TeamB.contains(args[1])){
+                        player.sendMessage(ChatColor.RED + "Gracz już jest w party!");
+                        return true;
+                    }
+                    Player invitedPlayer = Bukkit.getPlayer(args[1]);
+                    if(invitedPlayer != null && invitedPlayer.isOnline()){
+                        Main.TeamB.add(invitedPlayer.getName());
+                        Main.scoreTeamB.addEntry(args[1]);
+                        player.sendMessage(ChatColor.GREEN + "Dodałeś gracza " + invitedPlayer.getName() + "do party");
+                        return true;
+                    }
+                    player.sendMessage(ChatColor.RED + "Takiego gracza nie ma!");
+                    return true;
+                }
+                else{
+                    player.sendMessage(ChatColor.RED + "Nie jesteś kapitanem!");
+                    return true;
+                }
+            }
+
+            else if(args[0].equalsIgnoreCase("usun")){
+                if(args.length == 1){
+                    player.sendMessage(ChatColor.RED + "Poprawne użycie: /party usun <nick>");
+                    return true;
+                }
+
+                if(player.getName().equalsIgnoreCase(Main.CapitanA)){
+                    Player removedPlayer = Bukkit.getPlayer(args[1]);
+                    if(Main.TeamA.contains(removedPlayer.getName())){
+                        if(removedPlayer.getName().equalsIgnoreCase(Main.CapitanA)){
+                            player.sendMessage(ChatColor.RED + "Nie możesz usunąć samego siebie!");
+                        }
+                        Main.TeamA.remove(removedPlayer.getName());
+                        Main.scoreTeamA.removeEntry(args[1]);
+                        player.sendMessage(ChatColor.GREEN + "Usunąłeś gracza z party!");
+                    }
+                    else{
+                        player.sendMessage(ChatColor.RED + "Dany gracz nie znajduje się w party!");
+                    }
+                    return true;
+                }
+                else if(player.getName().equalsIgnoreCase(Main.CapitanB)){
+                    Player removedPlayer = Bukkit.getPlayer(args[1]);
+                    if(Main.TeamB.contains(removedPlayer.getName())){
+                        if(removedPlayer.getName().equalsIgnoreCase(Main.CapitanB)){
+                            player.sendMessage(ChatColor.RED + "Nie możesz usunąć samego siebie!");
+                        }
+                        Main.TeamB.remove(removedPlayer.getName());
+                        Main.scoreTeamB.removeEntry(args[1]);
+                        player.sendMessage(ChatColor.GREEN + "Usunąłeś gracza z party!");
+                    }
+                    else{
+                        player.sendMessage(ChatColor.RED + "Dany gracz nie znajduje się w party!");
+                    }
+                    return true;
+                }
+                else{
+                    player.sendMessage(ChatColor.RED + "Nie jesteś kapitanem!");
+                    return true;
+                }
+            }
+
+            else if(args[0].equalsIgnoreCase("list")){
+                if(Main.TeamA.contains(player.getName())){
+                    for(String nicks : Main.TeamA){
+                        player.sendMessage(ChatColor.GOLD + nicks);
+                    }
+                    return true;
+                }
+                else if(Main.TeamB.contains(player.getName())){
+                    for(String nicks : Main.TeamB){
+                        player.sendMessage(ChatColor.GOLD + nicks);
+                    }
+                    return true;
+                }
+                else{
+                    player.sendMessage(ChatColor.RED + "Nie znajdujesz się w żadnym party!");
+                    return true;
+                }
+            }
+            return true;
+        }
+        return true;
+    }
+
+    public void setScoreBoard(Player player){
+        Objective o = Main.board.registerNewObjective("ServerName", "dummy", "Test Server");
+        Score score  = o.getScore(ChatColor.GOLD + "Money: $" + ChatColor.GREEN + 1000);
+        score.setScore(3);
+        player.setScoreboard(Main.board);
+    }
+}
+
+

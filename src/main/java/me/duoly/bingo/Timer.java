3792f725e856755e1d8c55b1625dad9e73edc842
Misiris <pagrzej12@gmail.com>
2022-02-17 11:21:16 +0100

    rejoin teraz działa na serio

--- src/main/java/me/duoly/bingo/Timer.java
+++ src/main/java/me/duoly/bingo/Timer.java
@@ -21,9 +21,12 @@ public class Timer{
         this.plugin = plugin;
     }
     int timer;
+    int Rejointimer;
     int countID;
+    int RejoincountID;
     BossBar bar;
     double progress = 1.0;
+    double Rejoinprogress = 1.0;
     File LiveTime = new File("LiveTime.yml");
     FileConfiguration LiveTimeC = YamlConfiguration.loadConfiguration(LiveTime);
     public void addPlayer(Player player){
@@ -37,6 +40,7 @@ public class Timer{
         bar.setVisible(true);
     }
     double time;
+    double Rejointime;
 
     public void cast(){
             timer = config.getInt("time-of-game");
@@ -66,12 +70,12 @@ public class Timer{
                         for(String nick : Main.TeamA){
                             Player player = Bukkit.getPlayer(nick);
                             assert player != null;
-                            player.sendTitle(ChatColor.YELLOW+"Upłyneło połowe",ChatColor.YELLOW+"czasu",10,50,10 );
+                            player.sendTitle(ChatColor.YELLOW+"Upłyneła połowa",ChatColor.YELLOW+"czasu",10,50,10 );
                         }
                         for(String nick : Main.TeamB){
                             Player player = Bukkit.getPlayer(nick);
                             assert player != null;
-                            player.sendTitle(ChatColor.YELLOW+"Upłyneło połowe",ChatColor.YELLOW+"czasu",10,50,10 );
+                            player.sendTitle(ChatColor.YELLOW+"Upłyneła połowa",ChatColor.YELLOW+"czasu",10,50,10 );
                         }
                     }
                     if (timer == config.getInt("time-of-game")/10) {
@@ -143,97 +147,32 @@ public class Timer{
             }, 0L, 20L);
     }
     public void ReJoinCast(){
-        timer = LiveTimeC.getInt("time");
-        progress = LiveTimeC.getDouble("progress");
-        time = progress/timer;
-        countID=plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
+        Rejointimer = LiveTimeC.getInt("time");
+        Rejoinprogress = LiveTimeC.getDouble("progress");
+        Rejointime = Rejoinprogress/Rejointimer;
+        RejoincountID=plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
             public void run(){
-                if (progress > 0) {
-                    bar.setProgress(progress);
-                    bar.setTitle("Pozostały czas: "+timer/60+ ":"+ timer%60);
-                    progress=progress-time;
-                    timer--;
+                if (Rejoinprogress > 0) {
+                    bar.setProgress(Rejoinprogress);
+                    bar.setTitle("Pozostały czas: "+Rejointimer/60+ ":"+ Rejointimer%60);
+                    Rejoinprogress=Rejoinprogress-Rejointime;
+                    Rejointimer--;
                 } else {
-                    Bukkit.getScheduler().cancelTask(countID);
+                    Bukkit.getScheduler().cancelTask(RejoincountID);
                     bar.setVisible(false);
+                }
+                if (Rejointimer==1) {
+                    if (TasksChecking.ScoreTeamA == TasksChecking.ScoreTeamB) {
+                        Rejointimer = config.getInt("overtime");
+                        Rejoinprogress = 1.0;
+                        Rejointime = Rejoinprogress / Rejointimer;
+                    }
                 }
                 if (timer == config.getInt("time-of-game")/2) {
                     bar.setColor(BarColor.YELLOW);
-                    for(String nick : Main.TeamA){
-                        Player player = Bukkit.getPlayer(nick);
-                        assert player != null;
-                        player.sendTitle(ChatColor.YELLOW+"Upłyneło połowe",ChatColor.YELLOW+"czasu",10,50,10 );
-                    }
-                    for(String nick : Main.TeamB){
-                        Player player = Bukkit.getPlayer(nick);
-                        assert player != null;
-                        player.sendTitle(ChatColor.YELLOW+"Upłyneło połowe",ChatColor.YELLOW+"czasu",10,50,10 );
-                    }
                 }
                 if (timer == config.getInt("time-of-game")/10) {
                     bar.setColor(BarColor.RED);
-                    for(String nick : Main.TeamA){
-                        Player player = Bukkit.getPlayer(nick);
-                        assert player != null;
-                        player.sendTitle(ChatColor.RED+"Zostało "+timer/60+ ":"+ timer%60+ " sekund"," ",5,30,5 );
-                    }
-                    for(String nick : Main.TeamB){
-                        Player player = Bukkit.getPlayer(nick);
-                        assert player != null;
-                        player.sendTitle(ChatColor.RED+"Zostało "+timer+ " sekund"," ",5,30,5 );
-                    }
-                }
-                if (timer<=10){
-                    for(String nick : Main.TeamA){
-                        Player player = Bukkit.getPlayer(nick);
-                        assert player != null;
-                        if(timer<=3&timer>0){
-                            player.sendTitle(ChatColor.RED+"Zostało "+timer+ " sekund"," ",5,30,5 );
-                            player.playEffect(player.getLocation(), Effect.CLICK1,null);
-                        }
-                        else if(timer<=6&timer>0){
-                            player.sendTitle(ChatColor.YELLOW+"Zostało "+timer+ " sekund"," ",5,30,5 );
-                            player.playEffect(player.getLocation(), Effect.CLICK1,null);
-                        }
-                        else if(timer<=10&timer>0){
-                            player.sendTitle(ChatColor.GREEN+"Zostało "+timer+ " sekund"," ",5,30,5 );
-                            player.playEffect(player.getLocation(), Effect.CLICK1,null);
-                        }
-                    }
-                    for(String nick : Main.TeamB){
-                        Player player = Bukkit.getPlayer(nick);
-                        assert player != null;
-                        if(timer<=3&&timer>0){
-                            player.sendTitle(ChatColor.RED+"Zostało "+timer+ " sekund"," ",5,30,5 );
-                            player.playEffect(player.getLocation(), Effect.CLICK1,null);
-                        }
-                        else if(timer<=6&&timer>0){
-                            player.sendTitle(ChatColor.YELLOW+"Zostało "+timer+ " sekund"," ",5,30,5 );
-                            player.playEffect(player.getLocation(), Effect.CLICK1,null);
-                        }
-                        else if(timer<=10&&timer>0){
-                            player.sendTitle(ChatColor.GREEN+"Zostało "+timer+ " sekund"," ",5,30,5 );
-                            player.playEffect(player.getLocation(), Effect.CLICK1,null);
-                        }
-                    }
-                }
-                EndGame end = new EndGame(plugin);
-                if (timer==1){
-                    if(TasksChecking.ScoreTeamA==TasksChecking.ScoreTeamB){
-                        timer=config.getInt("overtime");
-                        progress=1.0;
-                        time = progress/timer;
-
-                        for(Player player : Bukkit.getOnlinePlayers()){
-                            player.sendTitle(ChatColor.RED+"Dogrywka!",ChatColor.RED+"Doliczono " + config.getInt("overtime")/60 + " minut",20,40,20);
-                            player.playEffect(player.getLocation(), Effect.ENDERDRAGON_GROWL,null);
-                        }
-                    }else end.End();
-                }
-                if (TasksChecking.ScoreTeamA==25 || TasksChecking.ScoreTeamB==25){
-                    Bukkit.getScheduler().cancelTask(countID);
-                    bar.setVisible(false);
-                    end.End();
                 }
             }
         }, 0L, 20L);
